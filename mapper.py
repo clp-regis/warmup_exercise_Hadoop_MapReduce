88459b51b4471884df5d251a8fd4281355633589
Christy Pearson <59812695+clp-regis@users.noreply.github.com>
2021-01-20 10:33:58 -0700

    Add files via upload

---
+++ mapper.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+import sys
+
+# input comes from STDIN (standard input)
+for line in sys.stdin:
+    line = line.strip()
+    line = line.split(",")
+
+    if len(line) >=4:
+        cause = line[1]
+        state = line[2]
+        deaths = line[3]
+
+        #print '%s\t%s' % (sex, age)
+        print(f'{state}\t{cause}\t{deaths}')
---
+++ reducer.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+#Reducer.py
+import sys
+
+cause_total = {}
+
+#Partitoner
+for line in sys.stdin:
+    line = line.strip()
+    state,cause, total = line.split('\t')
+
+    state_cause = cause +': ' + state
+
+    if state_cause in cause_total:
+        cause_total[state_cause].append(int(total))
+    else:
+        cause_total[state_cause] = []
+        cause_total[state_cause].append(int(total))
+
+
+#Reducer
+for state_cause in cause_total.keys():
+    ave_total = round(sum(cause_total[state_cause])*1.0 / len(cause_total[state_cause]))
+    print(f'{state_cause}\t{ave_total}')
