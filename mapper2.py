736843d92a3197571c876d10b89467d77c591f5d
Christy Pearson <cpearson002@regis.edu>
2021-01-28 21:17:20 +0000

    modified file

---
+++ mapper2.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+import sys
+
+# input comes from STDIN (standard input)
+for line in sys.stdin:
+    line = line.strip()
+    line = line.split(",")
+
+    if len(line) >=4:
+        cause = line[1]
+        state = line[2]
+        deaths = line[3]
+
+        if cause.lower() == 'diabetes':
+            print '%s\t%s\t%s' % (state, cause, deaths)
+       
